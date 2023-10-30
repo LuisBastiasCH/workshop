2421807755204093ddc49f635cafb60a5fdeb7c0
hvargas2007 <ext_hvargas@interbanking.com.ar>
2023-10-30 13:10:00 -0300

    feat(Update app) app.py Mon Oct 30 13:10:00 -03 2023

--- app.py
+++ app.py
@@ -8,7 +8,14 @@ hostname = socket.gethostname()
 
 # list of images:
 images = [
-    "https://media.giphy.com/media/lQDdDwdZpfYRn1MsJy/giphy.gif"
+    "https://media.giphy.com/media/BemKqR9RDK4V2/giphy.gif",
+    "https://media.giphy.com/media/citBl9yPwnUOs/giphy.gif",
+    "https://media.giphy.com/media/4no7ul3pa571e/giphy.gif",
+    "https://media.giphy.com/media/NAcNfRrU6f2bC/giphy.gif",
+    "https://media.giphy.com/media/ktcUyw6mBlMVa/giphy.gif",
+    "https://media.giphy.com/media/13rQ7rrTrvZXlm/giph.gif",
+    "https://media.giphy.com/media/o7OChVtT1oqmk/giphy.gif",
+    "https://media.giphy.com/media/NzQSHl01OBLkk/giphy.gif"
 ]
 
 @app.route('/')
@@ -24,14 +31,14 @@ def index():
 def status():
     code = urllib.request.urlopen("http://127.0.0.1:8082").getcode()
     if code == 200:
-        return jsonify(status="OK", version="1.0")
+        return jsonify(status="OK", version="1.2")
     else:
-        return jsonify(status="ERROR", version="1.0")
+        return jsonify(status="ERROR", version="1.2")
 
 @app.route('/version')
 def version():
     return jsonify(
-        version = "1.0",
+        version = "1.2",
         hostname = hostname,
         StatusCode = 200
         )
