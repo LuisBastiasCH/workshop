ffe173172633140483310eed80abdbf3a8288013
hvargas2007 <hermesvargas200720@gmail.com>
2024-05-13 15:08:04 -0300

    V2.0

--- app.py
+++ app.py
@@ -6,10 +6,25 @@ import socket
 app = Flask(__name__)
 hostname = socket.gethostname()
 
+# list of images:
+images = [
+    "https://media.giphy.com/media/BemKqR9RDK4V2/giphy.gif",
+    "https://media.giphy.com/media/citBl9yPwnUOs/giphy.gif",
+    "https://media.giphy.com/media/4no7ul3pa571e/giphy.gif",
+    "https://media.giphy.com/media/NAcNfRrU6f2bC/giphy.gif",
+    "https://media.giphy.com/media/ktcUyw6mBlMVa/giphy.gif",
+    "https://media.giphy.com/media/13rQ7rrTrvZXlm/giph.gif",
+    "https://media.giphy.com/media/o7OChVtT1oqmk/giphy.gif",
+    "https://media.giphy.com/media/NzQSHl01OBLkk/giphy.gif"
+]
+
 @app.route('/')
 def index():
+    url = random.choice(images)
     return render_template(
-        'index.html'
+        'index.html',
+        url=url,
+        hostname=hostname,
         )
 
 @app.route('/status')
@@ -22,11 +37,7 @@ def status():
 
 @app.route('/version')
 def version():
-    return jsonify(
-        version = "2.0",
-        hostname = hostname,
-        StatusCode = 200
-        )
+    return jsonify(version="2.0")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8082)
