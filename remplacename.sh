a6c82ab90d9ce436fcef8d73bc3f2e9aca24b3a8
LuisBastiasCH <161614391+LuisBastiasCH@users.noreply.github.com>
2024-05-14 12:35:13 -0300

    Update remplacename.sh

--- remplacename.sh
+++ remplacename.sh
@@ -1,12 +1,13 @@
-# Definir la ruta del archivo HTML
-html_file="/templates/index.html"
+#!/bin/bash
 
-# Crear un archivo temporal en el directorio temporal del sistema
-temp_file=$(mktemp)
+# URL del archivo HTML en GitHub
+github_html_url="https://raw.githubusercontent.com/tu_usuario/tu_repositorio/ruta/al/archivo/index.html"
+
+# Descargar el archivo HTML desde GitHub
+curl -sSL "$github_html_url" -o index.html
 
 # Modificar el archivo HTML usando sed
-# Por ejemplo, cambiaremos el contenido de un div con id "container"
-sed 's|<div id="container">Contenido anterior</div>|<div class="container">
+sed -i 's|<div id="container">Contenido anterior</div>|<div class="container">
         <h2>Random GIF</h2>
         <img src="{{url}}" alt="Random GIF" />
         <h4>This app is running on:</h4>
@@ -14,9 +15,15 @@ sed 's|<div id="container">Contenido anterior</div>|<div class="container">
         <p>Author: Hermes Vargas</p>
         <p>Version: 2.0.0</p>
         <p><a href="/status">Check server status</a></p>
-    </div>|' "$html_file" > "$temp_file"
-
-# Sobrescribir el archivo original con el archivo modificado
-mv "$temp_file" "$html_file"
+    </div>|' index.html
 
 echo "Archivo HTML modificado exitosamente"
+
+# Configurar git
+git config --global user.email "luis.bastias@cloudhesive.com"
+git config --global user.name "Luis Bastias"
+
+# Añadir cambios, hacer commit y push
+git add index.html
+git commit -m "Modificación automática del archivo HTML"
+git push origin master
