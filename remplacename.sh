f8dd30c18b5da85db7b5671c480c4f4a017d4388
LuisBastiasCH <161614391+LuisBastiasCH@users.noreply.github.com>
2024-05-14 12:19:37 -0300

    Update remplacename.shv3

--- remplacename.sh
+++ remplacename.sh
@@ -5,7 +5,7 @@ html_file="/templates/index.html"
 
 # Modificar el archivo HTML usando sed
 # Por ejemplo, cambiaremos el contenido de un div con id "miDiv"
-sed -i 's/<div id=""container"">Contenido anterior<\/div>/<div class="container">
+sed 's|<div id="container">Contenido anterior</div>|<div class="container">
         <h2>Random GIF</h2>
         <img src="{{url}}" alt="Random GIF" />
         <h4>This app is running on:</h4>
@@ -13,6 +13,9 @@ sed -i 's/<div id=""container"">Contenido anterior<\/div>/<div class="container"
         <p>Author: Hermes Vargas</p>
         <p>Version: 2.0.0</p>
         <p><a href="/status">Check server status</a></p>
-    </div>/' "$html_file"
+    </div>|' "$html_file" > "$html_file.tmp"
+
+# Sobrescribir el archivo original con el archivo modificado
+mv "$html_file.tmp" "$html_file"
 
 echo "Archivo HTML modificado exitosamente"
